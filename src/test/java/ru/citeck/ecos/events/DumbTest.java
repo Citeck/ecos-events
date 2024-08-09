ba6ad6f4decb8c29a7b7481e20f69e720e113ae7
Pavel Simonov <pavel.simonov@citeck.ru>
2024-08-09 13:21:28 +0700

    ECOSENT-2664 - Java 21. Release 1.2.0

---
+++ src/test/java/ru/citeck/ecos/events/DumbTest.java
@@ -0,0 +1,13 @@
+package ru.citeck.ecos.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class DumbTest {
+
+    @Test
+    public void test() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
